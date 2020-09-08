6b0b6dc0e5e8a359dd1e8af575dd7f1ec4292f6a
Bin Hyun <binhk1004@naver.com>
2020-09-09 00:44:28 +0900

    [Feature] Baseball crawler

--- app/main.py
+++ app/main.py
@@ -1,4 +1,31 @@
+import bs4
 from selenium import webdriver
+from selenium.webdriver.support.select import Select
 
-driver = webdriver.Chrome('/Users/binhk1004/Downloads/chromedriver')
-driver.get('http://www.naver.com')
+
+class BaseballCrawler():
+    def __init__(self, url):
+        driver = webdriver.Chrome('/Users/binhk1004/Downloads/chromedriver')
+        url = driver.get('https://www.koreabaseball.com/Default.aspx?vote=true')
+        self.__move_page()
+
+
+    def __move_page(self):
+        soup = bs4.BeautifulSoup(url, 'html.parser')
+        test = soup.find('body')
+        print(test)
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    BaseballCrawler()
