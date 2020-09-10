c003afa94fb7f0be1c402bdfc25d8e47d65c81bb
Bin Hyun <binhk1004@naver.com>
2020-09-10 22:56:46 +0900

    [Feature] move_page add function

--- app/main.py
+++ app/main.py
@@ -1,19 +1,25 @@
-import bs4
+from time import sleep
+
+from bs4 import BeautifulSoup
+import requests
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 
-
 class BaseballCrawler():
-    def __init__(self, url):
-        driver = webdriver.Chrome('/Users/binhk1004/Downloads/chromedriver')
-        url = driver.get('https://www.koreabaseball.com/Default.aspx?vote=true')
+    def __init__(self):
         self.__move_page()
 
 
     def __move_page(self):
-        soup = bs4.BeautifulSoup(url, 'html.parser')
-        test = soup.find('body')
-        print(test)
+        driver = webdriver.Chrome('/Users/binhk1004/Downloads/chromedriver')
+        url = driver.get('https://www.koreabaseball.com/Default.aspx?vote=true')
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        test = driver.find_element_by_xpath('//*[@id="lnb"]/li[3]')
+        test.click()
+        sleep(3)
+
+
 
 
 
