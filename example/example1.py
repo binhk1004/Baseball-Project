53a030cf4b09c7daf4d4932a9449d15a426a2dc8
Bin Hyun <binhk1004@naver.com>
2020-09-14 23:01:18 +0900

    [Feature] Created example1.py for velog

---
+++ example/example1.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+driver = driver = webdriver.Chrome('/Users/binhk1004/Downloads/chromedriver')
+url = driver.get('https://www.koreabaseball.com/Default.aspx?vote=true')
+
+
+
