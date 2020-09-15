a433869510f83ed9fff24c740ea41a7259b131b6
Bin Hyun <binhk1004@naver.com>
2020-09-15 23:38:00 +0900

    [Feature] Modified example1.py for velog

--- example/example1.py
+++ example/example1.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-driver = driver = webdriver.Chrome('/Users/binhk1004/Downloads/chromedriver')
-url = driver.get('https://www.koreabaseball.com/Default.aspx?vote=true')
+driver = webdriver.Chrome('/Users/binhk1004/Downloads/chromedriver')
+url = driver.get('https://www.naver.com/')
 
 
 
