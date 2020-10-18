d7f70a845e9b483157b6cc5d5b0fe8dab309bcd7
binhk1004 <binhk1004@naver.com>
2020-10-18 14:25:25 +0900

    [Feature] Add averages crawler]

--- example/example1.py
+++ example/example1.py
@@ -1,8 +1,16 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-driver = webdriver.Chrome('/Users/binhk1004/Downloads/chromedriver')
+driver = webdriver.Chrome('/Users/hyun/Downloads/chromedriver 2')
 url = driver.get('https://www.koreabaseball.com/Default.aspx')
 
 record_page = driver.find_element_by_xpath('//*[@id="lnb"]/li[3]')
 record_page.click()
+
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+
+averages = soup.select('ol.rankList')[0].select('li')
+
+for average in averages :
+    print(average.text)
